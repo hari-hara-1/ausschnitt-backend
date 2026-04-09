074ca1156af2df1afc7ecab0735ad0be3cf0051c
hari-hara-1 <hari.shenbagan@gmail.com>
2026-04-09 10:32:53 +0530

    Added health endpoint
    The app now contains the health endpoint to test the activity of the app

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 import "dotenv/config";
-import express from 'express';
-import authMiddleware from './middleware/auth.js';
-import authRouter from './routes/auth.routes.js'
-import snippetRouter from './routes/snippet.routes.js'
-import cors from 'cors';
+import express from "express";
+import authMiddleware from "./middleware/auth.js";
+import authRouter from "./routes/auth.routes.js";
+import snippetRouter from "./routes/snippet.routes.js";
+import healthRouter from "./routes/health.routes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRouter);
-app.use("/api/snippet", authMiddleware, snippetRouter)
+app.use("/api/snippet", authMiddleware, snippetRouter);
+app.use("/api", healthRouter);
 app.listen(process.env.PORT, () => {
-    console.log("Server started in PORT: "+process.env.PORT);
-})
+  console.log("Server started in PORT: " + process.env.PORT);
+});
